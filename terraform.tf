a68cffb67ed903070a5cc87b9466c4afdbbbcf94
Jeremy Walker <jeremwa@amazon.com>
2019-05-01 17:35:33 -0500

    Jenkinsfile work

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,10 @@
 provider "aws" {
     profile  = "${var.aws["profile"]}"
-    access_key = "${var.aws["aws_access_key"]}"
-    secret_key = "${var.aws["aws_secret_key"]}"
+    #access_key = "${var.aws["aws_access_key"]}"
+    #secret_key = "${var.aws["aws_secret_key"]}"
     region   = "${var.aws["region"]}"
+    access_key = "AKIAYD53B5EONNO27YXO"
+    secret_key = "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"
 }
 terraform {
     backend "s3" {
