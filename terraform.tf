4f206d744c984d4081bb017db00cd431606eeb11
Jeremy Walker <jeremwa@amazon.com>
2019-05-01 17:05:57 -0500

    Jenkinsfile work

--- terraform.tf
+++ terraform.tf
@@ -4,8 +4,5 @@ provider "aws" {
 }
 terraform {
     backend "s3" {
-        bucket="${var.backend_state_bucket}"
-        key="${var.backend_state_key}"
-        region="${var.aws["region"]}"
     }
 }
